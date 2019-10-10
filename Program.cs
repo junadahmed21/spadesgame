7ee901bdab1b0c203c62f797f54464a02671ec1e
junad ahmed <junad.ahmed21@outlook.com>
2019-10-10 19:52:21 -0400

    created solution

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spades
+{
+    class Program
+    {
+
+        public enum CardValue
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13
+        }
+        public enum CardSuit
+        {
+            Hearts = 1,
+            Spades = 2,
+            Clubs = 3,
+            Diamonds = 4
+        }
+    }
